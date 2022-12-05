a73e51bc539a8c6fbd0a01d490abaa20bbe2d85b
root <root@DESKTOP-S2J5AGU.localdomain>
2022-12-04 19:07:47 -0500

    Added python calcs files

--- TIMESHEET.md
+++ TIMESHEET.md
@@ -2,4 +2,6 @@
 CAD: 1pm-4pm
 Calcs:4pm-5pm
 
-
+12/4/22:
+Actuator Sys ID: 3-6pm
+Current Sense/Control for DC motor research/brainstorm: 6-7pm
---
+++ src/python/prop-sys-id.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+# matplotlib.use("QtAgg")
+
+file = open('data/prop-data.csv', 'r')
+rawdata = np.genfromtxt(file, delimiter=',', skip_header=2)
+print(rawdata)
+
+pwm = rawdata[:,0]
+force = rawdata[:,1]
+angvel = rawdata[:,2]
+
+plt.figure(1)
+plt.plot(pwm, force, 'r-', label='force(g)')
+plt.xlabel('pwm')
+plt.ylabel('force(g)')
+plt.title('PWM vs Force')
+plt.figure(2)
+plt.plot(pwm, angvel, 'b-', label='Omega(Rad/s)')
+plt.xlabel('pwm')
+plt.ylabel('Angvel (rad/s)')
+plt.title('PWM vs Angular Velocity')
+plt.figure(3)
+plt.plot(angvel, force, 'b-', label='Omega(Rad/s)')
+plt.xlabel('angvel')
+plt.ylabel('force')
+plt.title('Angular Velocity vs Force')
+plt.show()
