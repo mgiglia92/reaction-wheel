9bb5744fdb92c5dbfe80939f58c56174ca6eac68
Michael Giglia <michael.a.giglia@gmail.com>
2022-12-25 20:07:26 -0500

    Updates to GUI

--- src/python/gui/qt/graph_test.py
+++ src/python/gui/qt/graph_test.py
@@ -4,20 +4,53 @@ import pyqtgraph as pg
 import sys  # We need sys so that we can pass argv to QApplication
 import os
 from util.serial_comms import ArduinoComms
-from gui.qt.graph import Ui_MainWindow
+from gui.qt.ui import Ui_MainWindow
 import io
 from ruamel.yaml import YAML
 yaml = YAML()
+from threading import Thread
+from PyQt5.QtCore import QTimer
+from PyQt5.QtWidgets import QFileDialog
 
 class Ui_MainWindowFull(Ui_MainWindow):
     def setup(self):
         self.connectButton.clicked.connect(lambda: self.arduinoConnect(self.deviceName.text()))
+        self.yamlFileLocationButton.clicked.connect(self.yamlFileDialog)
+        self.dataFileLocationButton.clicked.connect(self.dataFileDialog)
         self.comms = ArduinoComms()
         self.loadYamlButton.clicked.connect(self.loadYaml)
         self.saveYamlButton.clicked.connect(self.saveYaml)
+        self.timer = QTimer(self)
+
+    def yamlFileDialog(self):
+        options = QFileDialog.Options()
+        directoryName= QFileDialog.getExistingDirectory(self, "Choose YAML Location")
+        self.yamlLocationLabel.setText(directoryName)
+
+    def dataFileDialog(self):
+        options = QFileDialog.Options()
+        directoryName= QFileDialog.getExistingDirectory(self, "Choose Data Location")
+        self.dataLocationLabel.setText(directoryName)
+
+    def plotfunc(self):
+        try:
+            # Process all the data in the queue
+            while not self.comms.dataQueue.empty():
+                self.comms.data.append(self.comms.processDataMessage(self.comms.dataQueue.get()))
+            # Clear graphs and plot the last 500 data points
+            self.graphWidget.clear()
+            self.graphWidget_2.clear()
+            self.graphWidget.plot(self.comms.data.ax[-500:-1])
+            self.graphWidget_2.plot(self.comms.data.wy[-500:-1])
+        except:
+            pass
+        self.timer.start(100)
+
 
     def arduinoConnect(self, port):
         self.comms.connect(port)
+        self.timer.timeout.connect(self.plotfunc)
+        self.timer.start(100)
     
     def loadYaml(self):
         filename = self.loadYamlFileName.text()
@@ -31,16 +64,21 @@ class Ui_MainWindowFull(Ui_MainWindow):
         buf = io.StringIO() #Get StringIO buffer
         yaml.dump(params, buf) # Dump the yaml file into the StringIO buffer
         self.yamlEditor.clear() # Clear all text from the editor
-        self.yamlEditor.appendPlainText(buf.getvalue()) # Put the yaml text
+        self.yamlEditor.append(buf.getvalue()) # Put the yaml text
+        file.close()
     
     def saveYaml(self):
-        pass
+        text = self.yamlEditor.toPlainText() # Get text for update yaml file
+        file = open(os.getcwd()+'/src/python/yaml/' + self.saveYamlFileName.text(), 'wb') # get the filename
+        file.write(bytes(text, 'utf-8'))# Write to the file
+        file.close()# Close the file 
         
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindowFull):
 
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
         self.setupUi(self)
+        self.setObjectName("Reaction Wheel GUI")
         self.setup()
 
 def main():
