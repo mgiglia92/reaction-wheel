29183c9a6bee80094c9f2cc200953bb77130b8e5
Michael Giglia <michael.a.giglia@gmail.com>
2022-12-30 14:40:41 -0500

    Pulled RCC library and reformatted for rxwheel instead

---
+++ src/arduino/rxwheel/src/rxwheel.h
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+/*
+ * rcc.h -- robotics crash course
+ *
+ * Copyright (C) 2022  Jacob Koziej <jacobkoziej@gmail.com>
+ */
+
+#ifndef CU_SUMMER_STEM_ROBOTICS_CRASH_COURSE_RCC_H
+#define CU_SUMMER_STEM_ROBOTICS_CRASH_COURSE_RCC_H
+
+
+#include "sensor/mpu6050.h"
+#include "sensor/odom.h"
+#include "util/array.h"
+#include "util/differentiator.h"
+#include "util/pid-control.h"
+#include "g_code_interpreter.h"
+
+
+#endif /* CU_SUMMER_STEM_ROBOTICS_CRASH_COURSE_RCC_H */
