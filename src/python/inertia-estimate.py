cf60f323e657cddaa4744f5b9db448b7ac1a2395
Michael Giglia <michael.a.giglia@gmail.com>
2023-01-03 22:04:53 -0500

    Scripts to analyze friction, inertia and inertia ratio data

---
+++ src/python/inertia-estimate.py
@@ -0,0 +1,81 @@
+"""
+NOTE: MPU6050 LPF set to '3' which is 42Hz bandwidth for gyroscope 4.8ms delay 
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from os import path
+import os
+import scipy.signal as signal
+
+Torque = 0.0547 * 68 #Newtons*mm
+
+Wn = 10
+sos = signal.butter(N=2, Wn=Wn, output='sos', fs=100, analog=False)
+
+dir = path.dirname(__file__)
+fname = os.path.join(dir, './data/weight-tests/vertical1.csv')
+file = open(fname,'r')
+data = np.genfromtxt(file, skip_header=1, delimiter=',')
+sample = data[:,0]
+ax = data[:,1]
+az = data[:,3]
+wsat = data[:,5]
+dt = data[:,7]
+count = data[:,9]
+count = count - count[0]
+
+time=np.empty((len(dt)),dtype=np.float32)
+for i,d in enumerate(dt):
+    time[i] = np.sum(dt[0:i])
+
+N = 51
+PPR = 12
+count = 360*count/(N*PPR) # convert to degrees
+countfilt = signal.sosfilt(sos, count)
+ww = np.gradient(count, time) # Convert to deg/sec
+wwfiltpre = np.gradient(countfilt,time)
+aw = np.gradient(ww, time)
+awfilt = np.gradient(wwfiltpre, time)
+wsatfilt = signal.sosfilt(sos,wsat)
+asat = np.gradient(wsat, time)
+asatfilt = np.gradient(wsatfilt, time)
+
+# Add theoretical friction acceleration to sattelite motion data
+B = -400
+#integrate acceleration with frictino removed
+wnof = np.empty((len(asat)), dtype=np.float32)
+for i,w in enumerate(wsat):
+        wnof[i] = wsat[i] + B*time[i]
+anof = np.gradient(wnof, time)
+jfilt = np.gradient(asatfilt,time)
+# separator = np.where(abs(jfilt)>5000)
+sep1 = signal.find_peaks(jfilt, 4000)
+sep2 = signal.find_peaks(-1*jfilt, 4000)
+
+plt.figure(0)
+plt.plot(time, wsat, 'r', label='sat ang vel')
+plt.plot(time, wsatfilt, 'b', label=f'sat ang vel (filtered, bw={Wn}Hz)')
+plt.plot(time, wnof, 'g', label='sat ang vel (no fric)')
+for s,d in zip(sep1[0],sep2[0]):
+    plt.axvline(time[s], color='black', linestyle=':')
+    plt.axvline(time[d], color='black', linestyle=':')
+plt.title("Angular Velocities")
+plt.xlabel("Time(s)")
+plt.ylabel("Angvel (deg/sec)")
+plt.legend()
+
+plt.figure(1)
+plt.title("Angular Acceleration")
+plt.xlabel("Time (s)")
+plt.ylabel("Angular Acceleration (deg/s2)")
+plt.plot(time, asat, 'r', label='ang acc')
+plt.plot(time, asatfilt, 'b', label=f'ang acc (filtered, bw={Wn}Hz)')
+plt.plot(time, anof, 'g', label='ang acc (no fric)')
+for s,d in zip(sep1[0],sep2[0]):
+    plt.axvline(time[s], color='black', linestyle=':')
+    plt.axvline(time[d], color='black', linestyle=':')
+plt.legend()
+plt.show()
+
+print("DF")
---
+++ src/python/inertia-ratio-estimate-remove-friction.py
@@ -0,0 +1,82 @@
+"""
+NOTE: MPU6050 LPF set to '3' which is 42Hz bandwidth for gyroscope 4.8ms delay 
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from os import path
+import os
+import scipy.signal as signal
+
+
+Wn1 = 5
+Wn2 = 10
+sos = signal.butter(N=2, Wn=Wn1, output='sos', fs=100)
+sos2 = signal.butter(N=2, Wn=Wn2, output='sos', fs=100)
+
+dir = path.dirname(__file__)
+fname = os.path.join(dir, './data/inertia-tests/vertical-inertia-test.csv')
+file = open(fname,'r')
+data = np.genfromtxt(file, skip_header=1, delimiter=',')
+sample = data[:,0]
+ax = data[:,1]
+az = data[:,3]
+wsat = data[:,5]
+dt = data[:,7]
+count = data[:,9]*-1 # Convert rotational direction
+count = count - count[0]
+
+time=np.empty((len(dt)),dtype=np.float32)
+for i,d in enumerate(dt):
+    time[i] = np.sum(dt[0:i])
+
+N = 51
+PPR = 6
+count = 360*count/(N*PPR) # convert to degrees
+countfilt = signal.sosfilt(sos, count)
+wsatfilt = signal.sosfilt(sos, wsat)
+wws = np.gradient(count, time) # Wheel rot rate in satellite frame
+wwsfilt = signal.sosfilt(sos,wws)
+aw = np.gradient(wws, time) # Acceleration of wheel in sat frame
+wwe = wws + wsat  # Get angular vel of wheel in inertial frame
+wwefilt = wwsfilt + wsatfilt
+# Add theoretical friction acceleration to sattelite motion data
+B = 350
+#integrate acceleration with frictino removed
+wsatnof = np.zeros((len(aw)), dtype=np.float32)
+wsatnof[0] = wsat[0]
+# Predict motion of system if friction was removed
+for i,w in enumerate(wsat):
+        if i == len(wsat)-1: break
+        wsatnof[i+1] = wsat[i] + B*time[i]
+
+
+wwenof = wws + wsatnof  # Get angular vel of wheel in inertial frame
+# Angular Accelerations
+awe = np.gradient(wwe,time)
+awefilt = np.gradient(wwefilt,time)
+asatfilt = np.gradient(wsatfilt,time)
+awenof = np.gradient(wwenof, time) # Wheel ang acc in inertial frame
+asatnof = np.gradient(wsatnof, time) # Sat ang acc with no friction
+    
+
+plt.figure(0)
+plt.plot(time, wwefilt, 'r:', markersize=3,label=f'Ang Vel Wheel (Inertial Frame, bw={Wn1}Hz)')
+plt.plot(time, wsatfilt, 'b:', label=f'Ang Vel Sat (Inertial Frame, bw={Wn1}Hz)')
+plt.plot(time, wwenof, 'r',label=f'Ang Vel Wheel (Inertial Frame, no fric)')
+plt.plot(time, wsatnof, 'b', label=f'Ang Vel Sat (Inertial Frame, no fric)')
+# plt.plot(time, wnof, 'c', label='Omega no Fric')
+plt.title("Angular Velocities in Inertial Frame")
+plt.xlabel("Time(s)")
+plt.ylabel("Angvel (deg/sec)")
+plt.legend()
+plt.figure(1)
+plt.plot(time, awefilt, 'r', label=f'Ang Acc Wheel (Inertial Frame, no fric, bw={Wn1}Hz)')
+plt.plot(time, asatfilt, 'b', label=f'Ang Acc Sat (Inertial Frame, no fric), bw={Wn1}Hz')
+plt.title("Angular Accelerations")
+plt.xlabel("Time(s)")
+plt.ylabel("Angacc (deg/sec2)")
+plt.legend()
+plt.show()
+
+print("DF")
